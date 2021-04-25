67ae5a826846d4f89f1b9a473c220c2acbed389b
johnerry <fashanutosin7@gmail.com>
2021-04-25 07:07:25 +0100

    Added files to main folder

---
+++ webpack.config.js
@@ -0,0 +1,16 @@
+const path = require('path');
+const { default : TimeFrame } = require("./src/timeframe");
+
+module.exports = {
+  entry: {
+    main: path.resolve(__dirname, './src/timeframe.js'),
+  },
+  output: {
+    filename: 'timeframe.bundle.js',
+    library: {
+      name: "TimeFrame",
+      type: "umd"
+    },
+    path: path.resolve(__dirname, 'dist')
+  }
+};
